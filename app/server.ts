33f33229ba171a53fa30b2bbaa403c7aabea8a82
30james00 <48206982+30james00@users.noreply.github.com>
2021-11-03 19:47:56 +0100

    Minor server.ts changes

--- app/server.ts
+++ app/server.ts
@@ -1,4 +1,4 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application } from 'https://deno.land/x/oak/mod.ts';
 const port = 8080;
 
 const date = new Date().toLocaleString('pl-PL');
@@ -6,11 +6,12 @@ const date = new Date().toLocaleString('pl-PL');
 // log date name and port (1a)
 console.log(`${date}\nMS\nPort: ${port}`);
 
-
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = ctx.request.ip;
+app.use(async (ctx) => {
+  const ip = ctx.request.ip;
+  const time = await getTime(ip);
+  ctx.response.body = `Your IP address: ${ip}\n${time}`;
 });
 
 await app.listen({ port });
